ee7f5abe1a49bffcbaaae583bac3a89a11f8dfd3
jrfiii <rob.frank.iii@gmail.com>
2024-05-06 16:40:57 -0500

    add testWords module for 9-letter word bank

---
+++ src/assets/testWords.ts
@@ -0,0 +1,9 @@
+const testWords: string[] = [
+  "conundrum",
+  "countdown",
+  "recognize",
+  "technique",
+  "flapjacks",
+];
+
+export default testWords[Math.floor(Math.random() * testWords.length)];
