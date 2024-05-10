892471cfe5ed1f5d726480c8600fe8ec47be7793
jrfiii <rob.frank.iii@gmail.com>
2024-05-09 19:15:07 -0500

    add react-router to project, create skeleton NavBar

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+import NavBar from "./routes/NavBar.tsx";
 import App from "./routes/App.tsx";
 import ErrorPage from "./error-page";
-import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <NavBar />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/letters',
+        element: <div>Placeholder for Letters game</div>,
+      },
+      {
+        path: '/numbers',
+        element: <div>Placeholder for Numbers game</div>,
+      },
+      {
+        path: '/finale',
+        element: <div>Move current component into its own component here</div>,
+      },
+      {
+        path: '/about',
+        element: <div>About section to go here</div>,
+      },
+    ]
   }
 ])
 
