338cb16f572a6e9756762e02f698f66c26a4808f
jrfiii <rob.frank.iii@gmail.com>
2024-05-29 15:54:55 -0500

    add react countdown circle timer package to project

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,9 +1,27 @@
 import {} from "react";
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+import RenderTime from "../components/RenderTime";
 
 function App() {
-
   return (
     <>
+      <div className="timer-wrapper">
+        <CountdownCircleTimer
+          isPlaying
+          duration={10}
+          colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+          colorsTime={[10, 6, 3, 0]}
+          onComplete={() => {
+            return {
+              shouldRepeat: true,
+              delay: 1,
+            };
+          }}
+        >
+          {RenderTime}
+        </CountdownCircleTimer>
+      </div>
       <h1>Conundrum</h1>
     </>
   );
