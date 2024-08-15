12537e7df9ac84595d7a03737440c2126f44c307
jrfiii <rob.frank.iii@gmail.com>
2024-08-15 18:37:27 -0500

    generate9Letters util function sketched out, not tested yet in component

--- src/utils/generate9LetterWord.ts
+++ src/utils/generate9LetterWord.ts
@@ -1,34 +1,39 @@
-function generate9LetterWord(orderedLetterArray: string[]) {
-  const vowels: object = {
-    A: 15,
-    E: 21,
-    I: 13,
-    O: 13,
-    U: 5,
-  };
-  const consonents: object = {
-    B: 2,
-    C: 3,
-    D: 6,
-    F: 2,
-    G: 3,
-    H: 2,
-    J: 1,
-    K: 1,
-    L: 5,
-    M: 4,
-    N: 8,
-    P: 4,
-    Q: 1,
-    R: 9,
-    S: 9,
-    T: 9,
-    V: 1,
-    W: 1,
-    X: 1,
-    Y: 1,
-    Z: 1,
-  };
+// generate9Letters to be called in Letters component
+// parameter letterOrder should be a 9-letter string composed of 'C's or 'V's, ex. 'CCVCCVCCV' or 'VVVCCCCCC'
+
+function generate9Letters(letterOrder: string) {
+  // const actualVowelDistribution: object = {
+  //   A: 15,
+  //   E: 21,
+  //   I: 13,
+  //   O: 13,
+  //   U: 5,
+  // };
+
+  let vowels: string = 'AAAEEEEIIIOOOUAAAEEEEIIIOOOU';
+  let consonants: string = 'BBCCCDDDDDDFFGGGHHJKLLLLLMMMMNNNNNNNNPPPPQRRRRRRRRRSSSSSSSSSTTTTTTTTTVWXYZ'
+
+  let nineLetters = '';
+
+  // function to generate random index of the above two strings, like drawing a random card from deck
+  const randomIndex = (vowelOrConsonant: string) => Math.floor(Math.random() * vowelOrConsonant.length);
+
+  //iterate over letterOrder argument
+  for (const char of letterOrder) {
+    if (char === 'C') {
+      //random index must be recalculated with each new char
+      const idx = randomIndex(consonants);
+      nineLetters += consonants[idx];
+      // consonants string must have recently-added letter removed so as to not affect letter frequency
+      consonants = consonants.substring(0, idx) + consonants.substring(idx + 1);
+    } else {
+      const idx = randomIndex(vowels);
+      nineLetters += vowels[idx];
+      vowels = vowels.substring(0, idx) + vowels.substring(idx + 1);
+    }
+  }
+
+  return nineLetters;
 }
 
-export default generate9LetterWord;
+export default generate9Letters;
