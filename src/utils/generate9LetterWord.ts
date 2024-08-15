e57e926964eaf39b8e30d23d6eb73c740d453bad
jrfiii <rob.frank.iii@gmail.com>
2024-08-14 21:58:56 -0500

    create generate9LetterWord util function for starting word round

---
+++ src/utils/generate9LetterWord.ts
@@ -0,0 +1,34 @@
+function generate9LetterWord(orderedLetterArray: string[]) {
+  const vowels: object = {
+    A: 15,
+    E: 21,
+    I: 13,
+    O: 13,
+    U: 5,
+  };
+  const consonents: object = {
+    B: 2,
+    C: 3,
+    D: 6,
+    F: 2,
+    G: 3,
+    H: 2,
+    J: 1,
+    K: 1,
+    L: 5,
+    M: 4,
+    N: 8,
+    P: 4,
+    Q: 1,
+    R: 9,
+    S: 9,
+    T: 9,
+    V: 1,
+    W: 1,
+    X: 1,
+    Y: 1,
+    Z: 1,
+  };
+}
+
+export default generate9LetterWord;
