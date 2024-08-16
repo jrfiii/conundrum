d3ff6c9ceea461bd76330b2eb0139a3388a2ec5c
jrfiii <rob.frank.iii@gmail.com>
2024-08-16 17:41:48 -0500

    testing util function in Letters component

--- src/routes/Letters.tsx
+++ src/routes/Letters.tsx
@@ -1,10 +1,30 @@
-import {} from "react";
+import { useState, useCallback, useEffect } from "react";
+import generate9Letters from "../utils/generate9LetterWord";
+
+// import { Reorder } from "framer-motion";
 
 function Letters() {
+  const [nineLetters, set9Letters] = useState("");
+  // const [letterArray, setLetterArray] = useState(['', '', '', '', '', '', '', '', '']);
+
+  // useEffect(() => {
+  //   setLetterArray(nineLetters.split(""));
+  // }, [nineLetters]);
+
+  const selectLetters = useCallback(
+    (letterOrder: string): void => set9Letters(generate9Letters(letterOrder)),
+    [],
+  );
+
+  // Testing  'CCVCCVCCV' or 'VVVCCCCCC'
+
   return (
     <>
       <h1>Letters Round</h1>
-      <div>Placeholder for Letters round</div>
+      <div>{nineLetters}</div>
+      <button onClick={() => selectLetters("CCVCCVCCV")}>
+        Testing CCVCCVCCV
+      </button>
     </>
   );
 }
