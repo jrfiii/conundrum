e5a26d646d27730583854f2147a2fb2328974f7b
jrfiii <rob.frank.iii@gmail.com>
2024-08-28 19:46:37 -0500

    transform letter into letter object with unique id in order to avoid react error with matching iterable keys

--- src/components/ReorderScramble.tsx
+++ src/components/ReorderScramble.tsx
@@ -1,19 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
 import { Reorder } from "framer-motion";
 import { v4 as uuidv4 } from "uuid";
 
-const ReorderScramble = () => {
-  const [testArr, setTestArr] = useState([1, 2, 3]);
+interface Props {
+  letterArray?: string[] | undefined;
+  // setLetterArray: Dispatch<SetStateAction<object[] | undefined>>
+}
+
+interface LtrObj {
+  value: string;
+  id: string;
+}
+
+const ReorderScramble = ({ letterArray }: Props) => {
+  const [testArr, setTestArr] = useState<LtrObj[]>();
+  // setLetterArray(letters.split("").map((ltr: string) => ({value: ltr, id: uuidv4()})));
+
+  useEffect(() => {
+    const ltrObjArr = letterArray?.map((ltr: string): LtrObj => ({
+      value: ltr,
+      id: uuidv4(),
+    }));
+    setTestArr(ltrObjArr);
+  }, [letterArray]);
 
   return (
     <>
-      <Reorder.Group values={testArr} onReorder={setTestArr}>
-        {testArr.map((val) => (
-          <Reorder.Item key={val} value={val}>
-            <div>{`${val}`}</div>
-          </Reorder.Item>
-        ))}
-      </Reorder.Group>
+      {testArr ? (
+        <Reorder.Group values={testArr} onReorder={setTestArr}>
+          {testArr?.map((val) => (
+            <Reorder.Item key={val.id} value={val}>
+              <div>{`${val.value} - ${val.id}`} type: {typeof val.id}</div>
+            </Reorder.Item>
+          ))}
+        </Reorder.Group>
+      ) : (
+        <div></div>
+      )}
     </>
   );
 };
