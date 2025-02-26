32025522cb77847d772b9d72b48d45414cb85e8c
jrfiii <rob.frank.iii@gmail.com>
2025-02-26 14:37:13 -0600

    add consonant vowel choices with verification of rules

--- src/routes/Letters.tsx
+++ src/routes/Letters.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import generate9Letters from "../utils/generate9LetterWord";
 import ReorderScramble from "../components/ReorderScramble";
 // import { v4 as uuidv4 } from "uuid";
@@ -9,28 +9,80 @@ import ReorderScramble from "../components/ReorderScramble";
  */
 
 function Letters() {
-  const [nineLetters, set9Letters] = useState("");
-  // const [letterArray, setLetterArray] = useState<object[]>();
+  const [consonantsVowels, setConsVowel] = useState("");
+  const [consButtonDisabled, setConsButtonDisabled] = useState(false);
+  const [vowelButtonDisabled, setVowelButtonDisabled] = useState(false);
 
-  const selectLetters = useCallback(
-    (letterOrder: string): void => {
-      const letters: string = generate9Letters(letterOrder);
-      set9Letters(letters);
-      // setLetterArray(letters.split("").map((ltr: string) => ({value: ltr, id: uuidv4()})));
-    },
-    [],
-  );
+  const [nineLetters, set9Letters] = useState("");
+
+  useEffect(() => {
+    //verification to keep letter choice in line with rules of game mode
+    const choiceLen = consonantsVowels.length;
+    const numCons = [...consonantsVowels.matchAll(/C/g)].length;
+    const numVowels = [...consonantsVowels.matchAll(/V/g)].length;
+
+    if (choiceLen < 7) {
+      numCons < 4 ? setConsButtonDisabled(false) : setConsButtonDisabled(true);
+      numVowels < 3
+        ? setVowelButtonDisabled(false)
+        : setVowelButtonDisabled(true);
+    } else {
+      setConsButtonDisabled(false);
+      setVowelButtonDisabled(false);
+    }
+  }, [consonantsVowels]);
+
+  const chooseLetterType = (type: string) => {
+    setConsVowel((prevState: string) => prevState + type);
+  };
+
+  const selectLetters = useCallback((letterOrder: string): void => {
+    const letters: string = generate9Letters(letterOrder);
+    set9Letters(letters);
+  }, []);
 
   // Testing  'CCVCCVCCV' or 'VVVCCCCCC'
   return (
     <>
       <h1>Letters Round</h1>
-      <div>nineLetters: {nineLetters}</div>
-      {/* <div>letterArray: {letterArray}</div> */}
-      <button onClick={() => selectLetters("CCVCCVCCV")}>
+      {nineLetters.length === 9 ? (
+        <ReorderScramble letterArray={nineLetters?.split("")} />
+      ) : (
+        <div className="board">
+          {consonantsVowels.split("").map((ltr) => (
+            <div className="tile placeholder">
+              {ltr === "C" ? "Consonant" : "Vowel"}
+            </div>
+          ))}
+        </div>
+      )}
+
+      {/* <div>nineLetters: {nineLetters}</div> */}
+      {/* <button onClick={() => selectLetters("CCVCCVCCV")}>
         Testing CCVCCVCCV
-      </button>
-      <ReorderScramble letterArray={nineLetters?.split('')} />
+      </button> */}
+      {/* <div>consonantsVowels: {consonantsVowels}</div> */}
+
+      {consonantsVowels.length < 9 ? (
+        <>
+          <button
+            disabled={consButtonDisabled}
+            onClick={() => chooseLetterType("C")}
+          >
+            Consonant
+          </button>
+          <button
+            disabled={vowelButtonDisabled}
+            onClick={() => chooseLetterType("V")}
+          >
+            Vowel
+          </button>
+        </>
+      ) : (
+        <button onClick={() => selectLetters(consonantsVowels)}>
+          Generate Letters
+        </button>
+      )}
     </>
   );
 }
